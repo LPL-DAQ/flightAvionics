5cb6cdea0f1f94e65085c0bd8d93f1baa9f9f08e
Frank Zhuang <frankzhu@usc.edu>
2023-01-11 13:38:28 -0800

    PT base functionality

---
+++ MCP3008.py
@@ -0,0 +1,25 @@
+class MCP3008:
+    def __init__(self,SPI_init):
+        self.SPI = SPI_init
+
+    def __readMCP3008(self, channel):
+        #gets the raw 10-bit value from the MCP3008 ADC
+
+        #request value from MCP3008 for channel number 'channel'
+        rawData = self.SPI.xfer([1, (8 + channel) << 4, 0])
+
+        #convert the two 8-bit replies to a single 10-bit reading
+        #processedData = 0...1023
+        processedData = ((rawData[1]&3) << 8) + rawData[2]
+
+        return processedData
+
+    def __dig2volts(self,digitalReading: int):
+        #converts digital value (0...1023) to voltage (0...5V)
+        voltage = (digitalReading * 5.365) / 1023
+        return voltage
+
+    def interrogate(self, channel):
+        data = self.__readMCP3008(channel)
+        voltage = self.__dig2volts(data)
+        return (voltage, data)
